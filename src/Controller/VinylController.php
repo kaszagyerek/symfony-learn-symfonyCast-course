02363aab38394c5d58d7381a5ef5e549db69739f
kaszagyerek <kasza.sandor@yahoo.com>
2022-07-22 16:35:14 +0200

    twig

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,15 +2,31 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class VinylController
+
+
+class VinylController extends  AbstractController
 {
     #[Route('/')]
     public function homepage() :Response
     {
-        return new Response('Title: Ks and Esz');
+
+        $alma = [
+          ['type' => 'nagy' , 'size' => 10 , 'owner' => 'KS'  ] ,
+          ['type' => 'nagy' , 'size' => 20 , 'owner' => 'KS'  ] ,
+          ['type' => 'nagy' , 'size' => 30 , 'owner' => 'KS'  ] ,
+          ['type' => 'nagy' , 'size' => 40 , 'owner' => 'KS'  ] ,
+          ['type' => 'nagy' , 'size' => 50 , 'owner' => 'KS'  ] ,
+          ['type' => 'nagy' , 'size' => 60 , 'owner' => 'KS'  ]
+        ];
+        return $this->render('vinyl/homepage.html.twig',[
+            'title' => 'KS ESZ L',
+            'szamok' => $alma
+        ]);
+
     }
 
     #[Route('/browse/{slug}')]
